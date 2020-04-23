c43fa1b0196783b17b96a130c105ebe72a40cd68
Joseph Diza <josephm.diza@gmail.com>
2020-04-23 14:13:25 -0700

    Fixed bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-git clone --bare git@bitbucket.org:durdn/cfg.git $HOME/.cfg
+# Configure system
+git clone https://gitlab.com/JMD_/systrap ~/.systrap -b $sys
+git clone --bare https://gitlab.com/JMD_/dotfiles $HOME/.cfg -b $sys
+git submodule add -f https://gitlab.com/JMD_/backgrounds ~/backgrounds
+
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
-mkdir -p .config-backup
-config checkout
+
+mkdir -p ~/.config-backup
+config checkout $sys
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
-config checkout
+config checkout $sys
 config config status.showUntrackedFiles no
